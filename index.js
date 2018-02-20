13d18ca4077e1cad8e5e0e1fe3a2a32905509ae2
Anna Yang <yanganna68@gmail.com>
2018-02-20 15:29:01 -0500

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+
+const sortedRobots = robots.map(function (robot) {
+  const decepticonOrNot = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+    alliance: decepticonOrNot ? 'decepticon' : 'autobot'
+  });
+});
+
+const coloredZebraStripes = zebraStripes.map(function (stripe, index) {
+  const blackOrNot = (index % 2) === 0;
+  return Object.assign({}, stripe, {
+    color: blackOrNot ? 'black' : 'white'
+  });
+});
